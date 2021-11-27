0235b11edfe899209d695c70a785f189ded58d89
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-11-27 23:56:58 +0600

    Saving to GitHub

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from api.models import Student
+from api.serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
+# Model Object - Single Student Data
+
+def student_detail(request):
+    stu = Student.objects.get(id=1)
+    serializer = StudentSerializer(stu)
+    json_data = JSONRenderer().render(serializer.data)
+    
+    return HttpResponse(json_data, content_type = 'application/json')
